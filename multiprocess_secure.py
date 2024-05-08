580a03532dc1349def52f7af1245e51667c491aa
davidralphm <davidralphwaldo12@gmail.com>
2024-05-08 13:09:10 +0700

    Modified multiprocess_secure.py

--- multiprocess_secure.py
+++ multiprocess_secure.py
@@ -6,6 +6,8 @@ import logging
 import multiprocessing
 from concurrent.futures import ProcessPoolExecutor
 from http import HttpServer
+import os
+import ssl
 
 httpserver = HttpServer()
 
@@ -46,21 +48,30 @@ def ProcessTheClient(connection,address):
 
 def Server():
 	the_clients = []
+
+#------------------------------
+	cert_location = os.getcwd() + '/certs/'
+	context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+	context.load_cert_chain(certfile=cert_location + 'domain.crt',
+								 keyfile=cert_location + 'domain.key')
+#---------------------------------
+
 	my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	my_socket.bind(('0.0.0.0', 8889))
+	my_socket.bind(('0.0.0.0', 8443))
 	my_socket.listen(1)
 
 	with ProcessPoolExecutor(20) as executor:
 		while True:
 				connection, client_address = my_socket.accept()
+				secure_connection = context.wrap_socket(connection, server_side=True)
 				#logging.warning("connection from {}".format(client_address))
-				p = executor.submit(ProcessTheClient, connection, client_address)
+				p = executor.submit(ProcessTheClient, secure_connection, client_address)
 				the_clients.append(p)
 				#menampilkan jumlah process yang sedang aktif
-				jumlah = ['x' for i in the_clients if i.running()==True]
-				print(jumlah)
+				#jumlah = ['x' for i in the_clients if i.running()==True]
+				#print(jumlah)
 
 
 
