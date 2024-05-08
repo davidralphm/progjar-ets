4367e978a56bb6cc334666d64d3af58ee3e560e0
davidralphm <davidralphwaldo12@gmail.com>
2024-05-08 16:50:07 +0700

    Modified multiprocess_secure.py

--- multiprocess_secure.py
+++ multiprocess_secure.py
@@ -16,46 +16,44 @@ httpserver = HttpServer()
 #maka class ProcessTheClient dirubah dulu menjadi function, tanpda memodifikasi behaviour didalamnya
 
 def ProcessTheClient(connection,address):
-		rcv=""
-		while True:
-			try:
-				data = connection.recv(32)
-				if data:
-					#merubah input dari socket (berupa bytes) ke dalam string
-					#agar bisa mendeteksi \r\n
-					d = data.decode()
-					rcv=rcv+d
-					if rcv[-2:]=='\r\n':
-						#end of command, proses string
-						#logging.warning("data dari client: {}" . format(rcv))
-						hasil = httpserver.proses(rcv)
-						#hasil akan berupa bytes
-						#untuk bisa ditambahi dengan string, maka string harus di encode
-						hasil=hasil+"\r\n\r\n".encode()
-						#logging.warning("balas ke  client: {}" . format(hasil))
-						#hasil sudah dalam bentuk bytes
-						connection.sendall(hasil)
-						print('done sending')
-						rcv=""
-						connection.close()
-						return
-				else:
-					break
-			except OSError as e:
-				pass
-		connection.close()
-		return
+	# Buat secure connection
+	connection = ssl.wrap_socket(connection, './certs/domain.key', './certs/domain.crt', True)
+
+	rcv=""
+
+	while True:
+		try:
+			data = connection.recv(32)
+			if data:
+				#merubah input dari socket (berupa bytes) ke dalam string
+				#agar bisa mendeteksi \r\n
+				d = data.decode()
+				rcv=rcv+d
+				if rcv[-2:]=='\r\n':
+					#end of command, proses string
+					#logging.warning("data dari client: {}" . format(rcv))
+					hasil = httpserver.proses(rcv)
+					#hasil akan berupa bytes
+					#untuk bisa ditambahi dengan string, maka string harus di encode
+					hasil=hasil+"\r\n\r\n".encode()
+					#logging.warning("balas ke  client: {}" . format(hasil))
+					#hasil sudah dalam bentuk bytes
+					connection.sendall(hasil)
+					print('done sending')
+					rcv=""
+					connection.close()
+					return
+			else:
+				break
+		except OSError as e:
+			pass
+	connection.close()
+	return
 
 
 
 def Server():
 	the_clients = []
-
-#------------------------------
-	cert_location = os.getcwd() + '/certs/'
-	context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-	context.load_cert_chain(certfile=cert_location + 'domain.crt', keyfile=cert_location + 'domain.key')
-#---------------------------------
 
 	my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -67,15 +65,11 @@ def Server():
 		while True:
 			connection, client_address = my_socket.accept()
 			try:
-				secure_connection = context.wrap_socket(connection, server_side=True)
 				#logging.warning("connection from {}".format(client_address))
-				p = executor.submit(ProcessTheClient, secure_connection, client_address)
-				the_clients.append(p)
+				p = executor.submit(ProcessTheClient, connection, client_address)
+				# the_clients.append(p)
 			except ssl.SSLError as essl:
 				print(str(essl))
-			#menampilkan jumlah process yang sedang aktif
-			#jumlah = ['x' for i in the_clients if i.running()==True]
-			#print(jumlah)
 
 
 
